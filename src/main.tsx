fd42d4a485f009b17517a093a4494f642bfa611c
DimitriTedom <dimitritedom@gmail.com>
2025-02-06 19:41:43 +0100

    feat(src): Setting up ThemeProvider with Shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
-import './index.css'
+import "./index.css";
+import { ThemeProvider } from "@/Providers/ThemeProvider";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppRouter />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AppRouter />
+    </ThemeProvider>
   </React.StrictMode>
 );
